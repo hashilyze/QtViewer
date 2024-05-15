bfec5caa5c57b32149b3e9aadeec8deafcab7510
Junsu Bae <hashilyze@gmail.com>
2024-05-15 23:07:57 +0900

    Feat: Tetrahedron

--- QtViewer/src/DrawComponent.cpp
+++ QtViewer/src/DrawComponent.cpp
@@ -6,15 +6,30 @@
 
 void DrawComponent::Init()
 {
-	pmp::read(mesh, "models\\bunny_stanford.obj");
+	/*pmp::read(mesh, "models\\bunny_stanford.obj");
 
 	pmp::vertex_normals(mesh);
 
-	std::cout << "#f " << mesh.n_faces() << "#y " << mesh.n_vertices() << std::endl;
+	std::cout << "#f " << mesh.n_faces() << "#y " << mesh.n_vertices() << std::endl;*/
 }
 
 void DrawComponent::Draw()
 {
+	// add 4 vertices 
+	const auto v0 = mesh.add_vertex(pmp::Point(0, 0, 0));
+	const auto v1 = mesh.add_vertex(pmp::Point(1, 0, 0));
+	const auto v2 = mesh.add_vertex(pmp::Point(0, 1, 0));
+	const auto v3 = mesh.add_vertex(pmp::Point(0, 0, 1));
+	// add 4 triangular faces
+	mesh.add_triangle(v0, v1, v3);
+	mesh.add_triangle(v1, v2, v3);
+	mesh.add_triangle(v2, v0, v3);
+	mesh.add_triangle(v0, v2, v1);
+
+	std::cout << "vertices: " << mesh.n_vertices() << std::endl;
+	std::cout << "edges: " << mesh.n_edges() << std::endl;
+	std::cout << "faces: " << mesh.n_faces() << std::endl;
+
 	auto normals = mesh.vertex_property<pmp::Normal>("v:normal");
 
 	for (auto f : mesh.faces()) {
