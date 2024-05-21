e2180f5a86341cb51fa3f248cb35ca3e3bd5b358
Junsu Bae <hashilyze@gmail.com>
2024-05-21 14:41:24 +0900

    Feat: Boundary

--- QtViewer/src/DrawComponent.h
+++ QtViewer/src/DrawComponent.h
@@ -1,18 +1,32 @@
 #pragma once
 #include "pmp/surface_mesh.h"
 
+
+typedef struct {
+    float h;
+    float s;
+    float v;
+} HSV;
+
 class DrawComponent
 {
 public:
     void Init();
-    void InitLoadFile();
     void InitMyMesh();
-    void InitProperties(pmp::SurfaceMesh *mesh);
+    void InitLoadFile();
 
     void Draw();
-    void DrawFace(pmp::Face f, bool isWire = false);
+    void DrawFace(pmp::SurfaceMesh* mesh, pmp::Face f, bool isWire = false);
     void AddAttributes();
 
-    pmp::SurfaceMesh mesh;
+    void CalIdx(int idx);
+
+    pmp::Color HSV2RGB(HSV hsv);
+
+    pmp::Color GetVertexColor(int vertexIndex, int maxIndex);
+
+    pmp::SurfaceMesh origMesh, mesh;
+
+    int arrowIdx = 0;
 };
 
